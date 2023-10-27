8eded0e9a6c525c60508e7f7cc203c4046a4122e
showd <345632828@qq.com>
2023-10-27 08:42:19 +0800

    add api demo

---
+++ app/edge-function/page.tsx
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+import { EdgeConfigValue, get } from "@vercel/edge-config";
+
+async function middleware() {
+  const greeting = await get("greeting");
+  return greeting;  
+  // return NextResponse.json(
+  //   { message: "Hello from middleware!" },
+  //   { status: 200 }
+  // );
+}
+
+async function About() {
+  let a: EdgeConfigValue | undefined;
+  a = await middleware();
+  console.log("-------------------");
+  console.log(new Date());
+  console.log(a);
+  
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex border border-indigo-600 h-8 hover:h-full">
+        About {a?.toString()}
+      </div>
+    </main>
+  );
+}
+
+export default About;
