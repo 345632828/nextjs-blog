fbe6813599cb0209da7d71b3a4a4cf555d2519c8
showd <345632828@qq.com>
2023-10-27 09:45:16 +0800

    updata 3

--- app/edge-function/page.tsx
+++ app/edge-function/page.tsx
@@ -1,14 +1,14 @@
-import { NextResponse } from "next/server";
-import { EdgeConfigValue, get } from "@vercel/edge-config";
+// import { NextResponse } from "next/server";
+// import { EdgeConfigValue, get } from "@vercel/edge-config";
 
-async function middleware() {
-  const greeting = await get("greeting");
-  return greeting;  
-  // return NextResponse.json(
-  //   { message: "Hello from middleware!" },
-  //   { status: 200 }
-  // );
-}
+// async function middleware() {
+//   const greeting = await get("greeting");
+//   return greeting;  
+//   return NextResponse.json(
+//     { message: "Hello from middleware!" },
+//     { status: 200 }
+//   );
+// }
 
 async function About() {
   // let a: EdgeConfigValue | undefined;
@@ -20,7 +20,7 @@ async function About() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex border border-indigo-600 h-8 hover:h-full">
-        About 
+        About 1111111111111111111
       </div>
     </main>
   );
